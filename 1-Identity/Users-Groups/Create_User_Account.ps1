4b6da5882e73a9f471220a07392e30483718d33c
Robin <robin@rwsdevops.co.uk>
2021-11-03 19:40:22 +0000

    First commit

---
+++ 1-Identity/Users-Groups/Create_User_Account.ps1
@@ -0,0 +1,25 @@
+<#
+This Script creates a user account 
+#>
+
+#Connect to Azure 
+Connect-AzAccount
+Get-AzContext
+Set-AZContext -Subscription "Azure subscription 1"
+
+#ImportModule
+Import-Module AzureAD
+
+
+#Create a password
+$PasswordProfile = New-Object -TypeName Microsoft.Open.AzureAD.Model.PasswordProfile
+$PasswordProfile.Password = "Cobbl3P0t"
+
+#Connect to Azure AD
+Connect-AzureAD
+New-AzureADUser -DisplayName "frodo.baggins" -PasswordProfile $PasswordProfile -UserPrincipalName "frodo@robin-spencer.co.uk" -AccountEnabled $true -MailNickName "frodo"
+
+
+
+
+
